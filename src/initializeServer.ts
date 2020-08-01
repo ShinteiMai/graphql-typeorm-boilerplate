f8699cb26b4c375d918dac67e1ff25c7143c3bfd
ShinteiMai <stevenhanselgo@gmail.com>
2020-08-01 22:06:34 +0700

    Decentralized resolvers and schemas into modules

--- src/initializeServer.ts
+++ src/initializeServer.ts
@@ -1,15 +1,27 @@
 import * as path from "path";
+import * as fs from "fs";
 
 import { GraphQLServer } from "graphql-yoga";
 import { importSchema } from "graphql-import";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
 
-import { resolvers } from "./resolvers";
 import { initializeTypeorm } from "./utils/initializeTypeorm";
+import { GraphQLSchema } from "graphql";
 
 export const server = async () => {
-  const typeDefs = importSchema(path.join(__dirname, "schema.graphql"));
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach((folder) => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
 
-  const instance = new GraphQLServer({ typeDefs, resolvers });
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
+  });
+
+  const stitchedSchemas: any = mergeSchemas({ schemas });
+  const instance = new GraphQLServer({ schema: stitchedSchemas });
 
   await initializeTypeorm();
   const port = process.env.NODE_ENV === "testing" ? 0 : 4000;
---
+++ src/modules/register/resolvers.ts
@@ -0,0 +1,21 @@
+import * as bcrypt from "bcryptjs";
+
+import { ResolverMap } from "../../types/graphql-utils";
+import { User } from "../../entity/User";
+
+export const resolvers: ResolverMap = {
+  Query: {
+    default: () => "Default query for GraphQL",
+  },
+  Mutation: {
+    register: async (_, { email, password }) => {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      const user = await User.create({
+        email,
+        password: hashedPassword,
+      });
+      await user.save();
+      return true;
+    },
+  },
+};
